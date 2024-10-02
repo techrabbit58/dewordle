b79a1e98f0c45c62a99bf121fe63419f0439c91d
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2024-10-02 10:52:02 +0200

    updated: dewordle.py

--- dewordle.py
+++ dewordle.py
@@ -1,21 +1,29 @@
 import pathlib
-import string
 import sys
 
 import typer
 
 
-def main(require: str = typer.Option(string.ascii_lowercase, case_sensitive=False),
-         exclude: str = typer.Option('', case_sensitive=False),) -> None:
+def main(require: str = typer.Option('', case_sensitive=False),
+         exclude: str = typer.Option('', case_sensitive=False),
+         nail: str = typer.Option('.....', case_sensitive=False),
+         deny: tuple[str, str, str, str, str] = typer.Option(tuple('.....'), case_sensitive=False)) -> None:
 
     with open(pathlib.Path(sys.argv[0]).cwd() / 'wordles.txt', encoding='utf-8') as fp:
         wordles = set(fp.read().strip().split())
 
-    required = set(require.lower())
-    excluded = set(exclude.lower()) - required
+    excluded = set(exclude.lower())
+    required = set(require.lower()) - excluded
+    nailed = (nail + '.....')[:5].lower()
+
+    nailed = [('.' if ch in excluded else ch) for ch in nailed]
+    required.update({ch for ch in nailed if ch != '.'})
+    denied = [(s.lower() if nailed[i] == '.' else '.') for i, s in enumerate(deny)]
 
     print(f'{excluded=}')
     print(f'{required=}')
+    print(f'{nailed=}')
+    print(f'{denied=}')
 
     candidates = set()
     for word in wordles:
@@ -30,7 +38,25 @@ def main(require: str = typer.Option(string.ascii_lowercase, case_sensitive=Fals
             candidates.add(word)
     wordles = candidates
 
-    print(wordles)
+    candidates = set()
+    for word in wordles:
+        for i in range(len(nailed)):
+            if nailed[i] != '.' and word[i] != nailed[i]:
+                break
+        else:
+            candidates.add(word)
+    wordles = candidates
+
+    candidates = set()
+    for word in wordles:
+        for i, d in enumerate(denied):
+            if word[i] in d:
+                break
+        else:
+            candidates.add(word)
+    wordles = candidates
+
+    print(sorted(wordles))
 
 
 if __name__ == '__main__':
