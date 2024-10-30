a6cca29cd0a573d3d686de90612df5a45e42b3de
Karl-Dieter Zimmer-Bentin <dzb@pruefbit.de>
2024-10-30 15:18:48 +0100

    updated: sandbox

--- sandbox/classdecorator.py
+++ sandbox/classdecorator.py
@@ -1,20 +1,24 @@
-def classdecorator[T](cls: T) -> T:
-    cls.num_pages = 175
-    for item, value in vars(cls).items():
-        print(item, '=', repr(value))
+def dto[T](cls: T) -> T:
+    args, obj_vars = [], []
+    for attr, type_ in cls.__annotations__.items():
+        obj_vars.append(f'    self.{attr} = {attr}\n')
+        args.append(f'{attr}: {type_.__name__}')
+
+    init_func = f'def __init__(self, {", ".join(args)}) -> None:\n'
+    for line in obj_vars:
+        init_func += line
+
+    print(init_func, globals(), locals())
+
     return cls
 
 
-@classdecorator
+@dto
 class Book:
-    author: str = 'Herbert Hobel'
-    title: str = 'Holzhandwerk im Wandel der Zeit'
-    num_pages: int
-
-    def get_author(self) -> str:
-        return self.author
+    author: str = 'Heinrich Hobel'
+    title: str
+    num_pages: int = '75'
 
 
 if __name__ == '__main__':
     book = Book()
-    book.get_author()
