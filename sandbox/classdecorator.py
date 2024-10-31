e171385e2f10c93d3c590349b1ccdeeb0d2e2719
techrabbit58 <dzb@pruefbit.de>
2024-10-31 18:47:31 +0100

    updates: sandbox

--- sandbox/classdecorator.py
+++ sandbox/classdecorator.py
@@ -1,14 +1,49 @@
+from typing import Any
+
+
+class DefaultSentinel:
+    pass
+
+
+class Field:
+    def __init__(self, name: str, type_: type, default: Any = DefaultSentinel()) -> None:
+        self.name = name
+        self.type_ = type_
+        self.default = default
+
+
 def dto[T](cls: T) -> T:
-    args, obj_vars = [], []
-    for attr, type_ in cls.__annotations__.items():
-        obj_vars.append(f'    self.{attr} = {attr}\n')
-        args.append(f'{attr}: {type_.__name__}')
+    args, obj_vars, fields = [], [], {}
+    for name, type_ in cls.__annotations__.items():
+        if name in cls.__dict__:
+            fields[name] = Field(name, type_, cls.__dict__[name])
+        else:
+            fields[name] = Field(name, type_)
 
-    init_func = f'def __init__(self, {", ".join(args)}) -> None:\n'
-    for line in obj_vars:
-        init_func += line
+    setattr(cls, '_FIELDS', fields)
 
-    print(init_func, globals(), locals())
+    for field in fields.values():
+        if isinstance(field.default, DefaultSentinel):
+            obj_vars.append(f'    self.{field.name} = {field.name}')
+            args.append(f'{field.name}: {field.type_.__name__}')
+
+    for field in fields.values():
+        if not isinstance(field.default, DefaultSentinel):
+            obj_vars.append(f'    self.{field.name} = {field.name}')
+            args.append(f'{field.name}: {field.type_.__name__} = {repr(field.default)}')
+
+    new_init = f"""
+def __init__(self{", " + ", ".join(args) if len(args) else ""}) -> None:
+{"\n".join(obj_vars) if len(obj_vars) else "    pass"}
+    """
+
+    exec(new_init, globals(), locals())
+
+    new_init_func = locals()['__init__']
+    new_init_func.__name__ = '__init__'
+    new_init_func.__qualname__ = f'{cls.__name__}.__init__'
+
+    cls.__init__ = new_init_func
 
     return cls
 
@@ -17,8 +52,9 @@ def dto[T](cls: T) -> T:
 class Book:
     author: str = 'Heinrich Hobel'
     title: str
-    num_pages: int = '75'
+    num_pages: int = 75
 
 
 if __name__ == '__main__':
-    book = Book()
+    book = Book('Holzbearbetung im Wandel der Zeit')
+    print(book.__dict__)
